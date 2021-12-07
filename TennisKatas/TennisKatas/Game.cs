6c773b20b663fc759e5989bca1e35dfef304ebaa
Pierre-Louis Serré <plouiserre@gmail.com>
2021-12-07 22:42:46 +0100

    start TDD with only games for the moment

---
+++ TennisKatas/TennisKatas/Game.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace TennisKatas
+{
+    public class Game
+    {
+        public string Score { get; set; }
+
+        public string Winner { get; set; }
+
+        public Game()
+        {
+        }
+
+        public void StartGame()
+        {
+            Score = "0-0";
+        }
+
+        public void PlayerOneScores()
+        {
+            if (Score == "0-0")
+            {
+                Score = "15-0";
+            }
+            else if (Score == "15-0")
+            {
+                Score = "30-0";
+            }
+            else
+            {
+                Score = "40-0";
+                Winner = "Player 1";
+            }
+        }
+
+        public void PlayerSecondScores()
+        {
+            Score = "30-15";
+        }
+    }
+}
---
+++ TennisKatas/TennisKatasTest/GameTest.cs
@@ -0,0 +1,94 @@
+using System;
+using TennisKatas;
+using Xunit;
+
+namespace TennisKatasTest
+{
+    public class GameTest
+    {
+        [Fact]
+        public void GameStart()
+        {
+            Game game = new Game();
+
+            game.StartGame();
+
+            Assert.Equal("0-0", game.Score);
+        }
+
+        [Fact]
+        public void PlayerOneScoresOneTime()
+        {
+            Game game = new Game();
+
+            game.StartGame();
+            game.PlayerOneScores();
+
+            Assert.Equal("15-0", game.Score);
+        }
+
+        [Fact]
+        public void PlayerOneScoresTwoTimes()
+        {
+            Game game = new Game();
+
+            game.StartGame();
+            game.PlayerOneScores();
+            game.PlayerOneScores();
+
+            Assert.Equal("30-0", game.Score);
+        }
+
+        [Fact]
+        public void PlayerOneScoresThreeTimes()
+        {
+            Game game = new Game();
+
+            game.StartGame();
+            game.PlayerOneScores();
+            game.PlayerOneScores();
+            game.PlayerOneScores();
+
+            Assert.Equal("40-0", game.Score);
+        }
+
+        [Fact]
+        public void PlayerOneScoresFourTimesWinGame()
+        {
+            Game game = new Game();
+
+            game.StartGame();
+            game.PlayerOneScores();
+            game.PlayerOneScores();
+            game.PlayerOneScores();
+
+            Assert.Equal("40-0", game.Score);
+            Assert.Equal("Player 1", game.Winner);
+        }
+
+        [Fact]
+        public void PlayerOneScoresTwoTimesPlayerSecondScoresOnce()
+        {
+            Game game = new Game();
+
+            game.StartGame();
+            game.PlayerOneScores();
+            game.PlayerOneScores();
+            game.PlayerSecondScores();
+
+            Assert.Equal("30-15", game.Score);
+        }
+        [Fact]
+        public void PlayerOneScoresFoursPlayerSecondScoresOnce()
+        {
+            Game game = new Game();
+
+            game.StartGame();
+            game.PlayerOneScores();
+            game.PlayerOneScores();
+            game.PlayerSecondScores();
+
+            Assert.Equal("30-15", game.Score);
+        }
+    }
+}
