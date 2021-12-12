f1caaa63b98d3a8a97d5cb412789fee5785ccaea
Pierre-Louis Serré <plouiserre@gmail.com>
2021-12-12 12:57:22 +0100

    init KeyGame TDD

---
+++ TennisKatas/TennisKatas/KeyGame.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TennisKatas
+{
+    public class KeyGame
+    {
+        public Player Player1 { get; set; }
+        public Player Player2 { get; set; }
+        public string Score { get; set; }
+
+        public KeyGame()
+        {
+            Player1 = new Player()
+            {
+                Identity = 1,
+                Score = 0
+            };
+            Player2 = new Player()
+            {
+                Identity = 2,
+                Score = 0
+            };
+        }
+
+        public void StartKeyGame()
+        {
+            Score = "0-0";
+        }
+    }
+}
---
+++ TennisKatas/TennisKatasTest/KeyGameTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TennisKatas;
+using Xunit;
+
+namespace TennisKatasTest
+{
+    public class KeyGameTest
+    {
+        public KeyGameTest()
+        {
+        }
+
+        [Fact]
+        public void KeyGameStart()
+        {
+            KeyGame keyGame = new KeyGame();
+
+            keyGame.StartKeyGame();
+
+            Assert.Equal(0, keyGame.Player1.Score);
+            Assert.Equal(0, keyGame.Player2.Score);
+            Assert.Equal("0-0", keyGame.Score);
+            Assert.False(keyGame.Player1.IsWinner);
+            Assert.False(keyGame.Player2.IsWinner);
+        }
+    }
+}
